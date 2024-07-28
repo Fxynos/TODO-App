46536dbae348b0f6a1573b7db683792a713c0fca
Exynos <sinerechka0000@gmail.com>
2024-07-29 00:02:45 +0700

    liquibase migration

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.utils.addToStdlib.cast
+import java.util.*
+
 plugins {
     kotlin("jvm") version "1.9.23"
     id("org.springframework.boot") version "2.7.3"
+    id("org.liquibase.gradle") version "2.2.0"
 }
 
 group = "com.dn.todo"
@@ -14,8 +18,31 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.3")
     implementation("org.springframework.boot:spring-boot-starter-validation:2.7.3")
     runtimeOnly("com.h2database:h2:2.3.230")
+
+    liquibaseRuntime("com.h2database:h2:2.3.230")
+    liquibaseRuntime("org.liquibase:liquibase-core:4.20.0")
+    liquibaseRuntime("info.picocli:picocli:4.6.3")
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+val applicationProperties = Properties().apply {
+    file("src/main/resources/application.properties").inputStream().use { load(it) }
+}
+
+liquibase {
+    activities {
+        register("main") {
+            arguments = mapOf(
+                "changelogFile" to "changelog.sql",
+                "url" to (
+                    properties["db.h2.address"] ?:
+                    applicationProperties["db.h2.address"] ?:
+                    throw IllegalArgumentException("define db.h2.address property")
+                )
+            )
+        }
+    }
 }
